d386c239633847ae74d8ec51242b73c2cb195cdf
syou551 <hiroto.0108si+@gmail.com>
2024-05-01 15:13:43 +0900

    complete ls api

--- back/main.go
+++ back/main.go
@@ -2,25 +2,68 @@ package main
 
 import (
 	//"database/sql"
-	//"encoding/json"
+	"encoding/json"
 	"os/exec"
+	"os"
     "fmt"
     "log"
+	"strings"
     "net/http"
 )
 
-func hello_world(w http.ResponseWriter, r *http.Request){
-	out, err := exec.Command("ls", "../").Output()
+type Datas struct{
+	Name string `json:"Name"`
+	Type string `json:"Type"`
+}
 
+func getLsRes(w http.ResponseWriter, r *http.Request){
+	dirPath := "../"
+
+	out, err := exec.Command("ls", dirPath).Output()
 	if err != nil {
 		log.Fatal("cmd execute; ", err)
+		w.WriteHeader(http.StatusNoContent)
 	}
-    fmt.Fprintf(w,string(out))
+    fmt.Println(string(out))
 	//outをJsonにして返す
+	files := strings.Split(string(out),"\n")
+	res := []Datas{}
+	for _, item := range files{
+		data := Datas{}
+		if item == "" {continue}
+		fInfo, err := IsDirectory(dirPath+item)
+		if err != nil {
+			fmt.Println("Error")
+			continue
+		}
+		data.Name = item
+
+		if fInfo{ 
+			data.Type = "Dir"
+		}else{
+			data.Type = "File"
+		}
+		res = append(res, data)
+	}
+	//Response Header
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	response, _ := json.Marshal(res)
+	w.Write(response)
+}
+
+// 指定されたファイル名がディレクトリかどうか調べる
+func IsDirectory(name string) (isDir bool, err error) {
+	fInfo, err := os.Stat(name) // FileInfo型が返る。
+	if err != nil {
+			return false, err // もしエラーならエラー情報を返す
+	}
+	// ディレクトリかどうかチェック
+	return fInfo.IsDir(), nil
 }
 
 func main() {
-    http.HandleFunc("/api/", hello_world)
+    http.HandleFunc("/api/ls", getLsRes)
 
     // クロージャを渡しても良い
     // http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
