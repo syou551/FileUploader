dd794b94c5bff491c8c16673d5f43a25fbd96ed0
syou551 <hiroto.0108si+@gmail.com>
2025-02-02 18:22:02 +0900

    fix singIn

--- front/dir-share/src/app/lib/auth.ts
+++ front/dir-share/src/app/lib/auth.ts
@@ -64,14 +64,6 @@ export const authOptions : NextAuthOptions = {
         }
     },*/
     callbacks: {
-        async signIn({account, profile}:{account: Account | null; profile?: Profile | undefined;}){
-            const isAccount = account && profile
-            if (isAccount && account.provider === "keycloak") {
-                // return profile.email_verified && profile.email.endsWith("@example.com")
-                return true
-            }
-            return true // Do different verification for other providers that don't have `email_verified`
-        },
         async jwt({ token }) {
           token.userRole = "admin"
           return token
